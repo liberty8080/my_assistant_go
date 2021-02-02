a59a7171dc6b3a35a7b0359261e5653fa556b5d0
liberty8080 <zhaojiawei233@gmail.com>
2021-02-02 11:08:00 +0800

    新增过期时间查询

--- util/ipUtil.go
+++ util/ipUtil.go
@@ -1,9 +1,13 @@
 package util
 
 import (
+	"encoding/base64"
+	"encoding/json"
 	"fmt"
 	"io/ioutil"
+	"log"
 	"net/http"
+	"strings"
 )
 
 func download(url string) {
@@ -26,12 +30,61 @@ func download(url string) {
 	fmt.Println(string(body))
 }
 
+// 获取当前公网ip
 func GetPublicIP() string {
-	res, err := http.Get("http://ip.3322.net/")
+	res, err := http.Get("http://ip.42.pl/raw")
 	if err != nil {
 		panic(err)
 	}
 	defer res.Body.Close()
 	s, err := ioutil.ReadAll(res.Body)
 	return string(s)
+}
+
+// 同步ddns
+func SyncDDNS() string {
+	return ""
+}
+
+type VmessObj struct {
+	Host       string `json:"host"`
+	Path       string `json:"path"`
+	TLS        string `json:"tls"`
+	VerifyCert bool   `json:"verify_cert"`
+	Add        string `json:"add"`
+	Port       int    `json:"port"`
+	Aid        int    `json:"aid"`
+	Net        string `json:"net"`
+	HeaderType string `json:"headerType"`
+	V          string `json:"v"`
+	Type       string `json:"type"`
+	Ps         string `json:"ps"`
+	Remark     string `json:"remark"`
+	ID         string `json:"id"`
+	Class      int    `json:"class"`
+}
+
+func Expire() string {
+	res := Get("https://subscribe.zealingcloud.info/link/Zxv1RYVrJ7MXhSFS?sub=3")
+	encodedLinks, err := base64.StdEncoding.DecodeString(res)
+	if err != nil {
+		log.Panic("解码失败!")
+	}
+	var result = ""
+	for _, encoded := range strings.Split(string(encodedLinks), "\n") {
+		if len(encoded) != 0 {
+			link, err := base64.StdEncoding.DecodeString(strings.Replace(encoded, "vmess://", "", 1))
+			if err != nil {
+				log.Panic("vmess解码失败! vmess:" + string(link))
+			}
+			log.Println(string(link))
+			v := VmessObj{}
+			_ = json.Unmarshal(link, &v)
+			if strings.Contains(v.Remark, "剩余流量") || strings.Contains(v.Remark, "过期时间") {
+				log.Println(v.Remark)
+				result += v.Remark + "\n"
+			}
+		}
+	}
+	return result
 }
